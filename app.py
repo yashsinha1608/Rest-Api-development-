e6ca23bccdaa2ed0c5bc12c2594e01fc4c2251cb
YASH <yash1608.sinha@gmail.com>
2026-04-21 01:07:41 +0530

    S

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask 
+app = Flask(__name__)
+
+@app.route("/") #decoraors ie app.route naam ka eak function hai aur uske andar joh abhi hum likhe hai voh sab paas hoga
+def welcome():
+    return "hello" 
+
+@app.route("/home") 
+def home():
+    return "this is home page" 
+@app.route("/chill") 
+def chill():
+    return "this is just a  page" 

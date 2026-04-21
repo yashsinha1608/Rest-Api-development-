5777e11be75e50ba4152405d2b56dcddb8a760c9
YASH <yash1608.sinha@gmail.com>
2026-04-22 02:04:55 +0530

    read operation

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,6 +1,6 @@
 from app import app
 from model.user_model import user_model #importing user_model class from user_model.py file
 obj=user_model()
-@app.route("/user/signup") 
-def user_signup_controller():
-    return obj.user_signup_model() 
+@app.route("/user/getall") 
+def user_getall_controller():
+    return obj.user_getall_model()  
--- model/user_model.py
+++ model/user_model.py
@@ -1,3 +1,19 @@
+import mysql.connector
+import json
 class user_model():
-    def user_signup_model(self):
-        return"This is user_signup_model" 
+    def __init__(self):  #constructor for connection establishment 
+        try:
+            self.con=mysql.connector.connect(host="localhost",user="root",password="",database="flask_tutorial") #connect btw pyhon and mysql 
+            self.cur=self.con.cursor(dictionary=True) 
+            print("succesful")       
+        except: 
+            print("some error")
+
+    def user_getall_model(self):
+        self.cur.execute("SELECT * FROM users")
+        result=self.cur.fetchall()
+        if len(result)>0:
+            return json.dumps(result) 
+        else: 
+            return "no data found"  
+       

6677507e216ebf885cff00332decf133992ebac7
YASH <yash1608.sinha@gmail.com>
2026-05-02 00:56:11 +0530

    access control header

--- model/user_model.py
+++ model/user_model.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import json
-
+from flask import make_response
 class user_model():
     def __init__(self):
         try:
@@ -15,9 +15,11 @@ class user_model():
         self.cur.execute("SELECT * FROM users")
         result = self.cur.fetchall()
         if len(result) > 0:
-            return {"payload":result}
+            res =make_response({"payload":result},200)
+            res.headers['Access-Control-Allow-Origin']="*"
+            return res
         else:
-            return {"message":"no data found"}
+            return make_response({"message":"no data found"},204) # 204 is a http code which does not need a body  
 
     def user_addone_model(self, data):
         name = data['name']
@@ -26,20 +28,20 @@ class user_model():
         role = data['role']
         password = data['password']
         self.cur.execute(f"INSERT INTO users(name,email,phone,role,password) VALUES('{name}','{email}','{phone}','{role}','{password}')")
-        return {"message":"user added"}
+        return make_response({"message":"user added"},201)
 
     def user_update_model(self, data):   # ← must be indented inside class!
             self.cur.execute(f"UPDATE users SET name='{data['name']}',email='{data['email']}',phone='{data['phone']}',role='{data['role']}',password='{data['password']}' WHERE id={data['id']} ")
          #   self.con.commit()
             if self.cur.rowcount>0: #if rowcount=0 ie same data is given 
-                return {"message":"user updated successfully"}
+                return make_response({"message":"user updated successfully"},201)
             else:
-                return {"message":"nothing to update"}
+                return make_response({"message":"nothing to update"},202)
 
     def user_delete_model(self, id):   # ← must be indented inside class!
             self.cur.execute(f"DELETE FROM users WHERE id={id}")
          #   self.con.commit()
             if self.cur.rowcount>0: #if rowcount=0 ie same data is given 
-                return {"message":"user deleted successfully"}
+                return make_response({"message":"user deleted successfully"},200)
             else:
-                return {"message":"nothing to delete"}
+                return make_response({"message":"nothing to delete"},204)
